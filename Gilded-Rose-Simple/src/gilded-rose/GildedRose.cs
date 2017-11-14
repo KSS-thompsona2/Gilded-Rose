aea83fb914275aa4a3f5dc8142f1cd336beca39d
KSS-thompsona2 <thompsona@kalibrate.com>
2017-11-14 16:12:43 +0000

    Initial Commit

---
+++ Gilded-Rose-Simple/src/gilded-rose/GildedRose.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gilded_rose
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        private static void UpdateItem(Item item)
+        {
+            switch (item.Name)
+            {
+                case "Aged Brie":
+                    item.SellIn--;
+                    UpdateQuality(item, 1);
+                    break;
+
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    item.SellIn--;
+                    if (item.SellIn < 0)
+                    {
+                        UpdateQuality(item, -item.Quality);
+                    }
+                    else if (item.SellIn <= 5)
+                    {
+                        UpdateQuality(item, 3);
+                    }
+                    else if (item.SellIn <= 10)
+                    {
+                        UpdateQuality(item, 2);
+                    }
+                    else
+                    {
+                        UpdateQuality(item, 1);
+                    }
+                    break;
+
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+
+                default:
+                    item.SellIn--;
+                    if (item.Name.StartsWith("Conjured", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        UpdateQuality(item, -2);
+                    }
+                    else
+                    {
+                        UpdateQuality(item);
+                    }
+                    break;
+            }
+
+        }
+
+        public void UpdateItems()
+        {
+            foreach (Item item in Items)
+            {
+                UpdateItem(item);
+            }
+        }
+
+        private static void UpdateQuality(Item item, int increment = -1)
+        {
+            item.Quality = Math.Max(0, item.Quality + increment);
+            item.Quality = Math.Min(50, item.Quality);
+        }
+    }
+}
---
+++ Gilded-Rose-Simple/test/gilded-rose-test/UnitTests.cs
@@ -0,0 +1,59 @@
+﻿using gilded_rose;
+using Xunit;
+
+namespace gilded_rose_test
+{
+    public class UnitTests
+    {
+        [Theory]
+        [InlineData("+5 Dexterity Vest", 10, 20, 9, 19)]
+        [InlineData("Conjured Mana Cake", 3, 6, 2, 4)]
+        [InlineData("Aged Brie", 2, 0, 1, 1)]
+        [InlineData("Elixir of the Mongoose", 5, 7, 4, 6)]
+        [InlineData("Sulfuras, Hand of Ragnaros", 0, 80, 0, 80)]
+        [InlineData("Sulfuras, Hand of Ragnaros", -1, 80, -1, 80)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 15, 20, 14, 21)]
+        public void BasicTest(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+        {
+            var items = new[] { new Item(){
+                    Name= name,
+                    SellIn = sellIn,
+                    Quality = quality } };
+            new GildedRose(items).UpdateItems();
+            Assert.Equal(expectedSellIn, items[0].SellIn);
+            Assert.Equal(expectedQuality, items[0].Quality);
+        }
+
+        [Theory]
+        [InlineData("+5 Dexterity Vest", 1, 0, 0, 0)]
+        [InlineData("Aged Brie", 2, 50, 1, 50)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 10, 49, 9, 50)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 5, 49, 4, 50)]
+        public void EdgeCasesTest(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+        {
+            var items = new[] { new Item(){
+                    Name= name,
+                    SellIn = sellIn,
+                    Quality = quality } };
+            new GildedRose(items).UpdateItems();
+            Assert.Equal(expectedSellIn, items[0].SellIn);
+            Assert.Equal(expectedQuality, items[0].Quality);
+        }
+
+        [Theory]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 10, 40, 9, 42)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 5, 40, 4, 43)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 0, 49, -1, 0)]
+        [InlineData("Conjured", 10, 10, 9, 8)]
+        public void ComplexItemsTest(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+        {
+            var items = new[] { new Item(){
+                    Name= name,
+                    SellIn = sellIn,
+                    Quality = quality } };
+            new GildedRose(items).UpdateItems();
+            Assert.Equal(expectedSellIn, items[0].SellIn);
+            Assert.Equal(expectedQuality, items[0].Quality);
+        }
+    }
+}
